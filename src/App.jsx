7b24767d5a857f2d9a187bc5b328f73b149a24cd
unknown <karthikeya3762@gmail.com>
2023-12-13 19:58:02 +0530

    add a dyanamic content to acomponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 function App(){
-
+    const now =new Date()
     console.log("Hello from component");
+
+    let a=10;
+
+    let b=15;
 
   return(
 
     <div>
-      <h1>hello world</h1>
+      <h1>hello world is{now.toString()}</h1>
+      <p>{a} plus {b} is {a+b}</p>
     </div>
   )
 }
