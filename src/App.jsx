4b5b1bf140e8b9e1f14f69e7a5fa0fffd6357e4f
unknown <karthikeya3762@gmail.com>
2023-12-14 21:19:54 +0530

    create a component

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,5 @@
-
-function Hello(props){
-  console.log(props);
-return(
-
-  <>
-  <p>Hello {props.userName} and your age is {props.age}</p>
- 
-  </>
-  
-)
-
-
-
-}
-
-
-
 function App(){
-   
-
-  return(
-    <div>
-      <h1>Greatings</h1>
-      <Hello userName={"Karthi"} age={25}/>
-      <Hello userName={"KarthiKeyan"} age={26}/>
-    </div>
-    
-  )
+  return <p>Helloworld</p>
 }
 
 export default App;
