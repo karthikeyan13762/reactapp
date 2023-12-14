04e63eff246cb8a1cf5429edbc176260e5d1bb46
unknown <karthikeya3762@gmail.com>
2023-12-14 21:34:40 +0530

    multiple components and data passing as props

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+function Hello(props){
+
+  return (
+
+    <p>Hello {props.name} your age is {props.age}</p>
+  )
+}
+
+
+
+
 const App=()=>{
   return (
     <div>
-      <h1>App Component</h1>
-      <p>Hello World</p>
+      <h1>Greatings</h1>
+      <Hello nage={"Karthi"} age={25}/>
+      <Hello name={"Karthikeyan"} age={26}/>
     </div>
   )
 }
