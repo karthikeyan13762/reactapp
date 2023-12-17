5a1b80b95b5ec706c9967a242cb1a2946bf55d1a
unknown <karthikeya3762@gmail.com>
2023-12-17 20:11:04 +0530

    functional component vs class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+// import React from 'react' Not necessary from react17
+// function component
+// function App() {
+//   return (
+//     <div>Hello World</div>
+//   )
+// }
+// export default App
+import { Component } from "react";
 
-function App() {
-  return (
-    <div>App</div>
-  )
+
+
+
+class App extends Component{
+
+  render(){
+    return(
+
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    )
+  }
+
 }
 
-export default App
+export default App;
