5b83ff211d9432588a9d7cd624302e720b914971
unknown <karthikeya3762@gmail.com>
2023-12-25 20:06:45 +0530

    Adding React Router Dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,42 @@
-import React, { useState } from 'react'
+import React from 'react'
+import {Link, Route, BrowserRouter as Router, Routes} from 'react-router-dom'
 import Home from './Components/Home'
-
-
 import Notes from './Components/Notes'
-
 import Users from './Components/Users'
-
 function App() {
 
-  const[page,setPage]=useState('home')
-  const toChange=(page)=>(event)=>{
-    event.preventDefault()
-    setPage(page)
+  const padding={
+    padding:10,
+    margin:5,
+    backgroundColor:"gray",
+    color:'yellow',
+    textDecoration:'none'
   }
-
-   const padding={
-    padding:5,
-   }
-
-   function content(){
-    if(page=='home'){
-     return <Home/>
-    }else if(page=='notes'){
-      return <Notes/>
-    }else if(page=='users'){
-      return <Users/>
-    }
-
-   }
-   
+  const styles={
+    backgroundColor:"black",
+    color:"white",
+    width:100+"vw",
+    height:55+"vh",
+  }
   return (
-    <div>
-   
-        <a href='' style={padding} onClick={toChange('home')}>Home</a>
-        <a href='' style={padding} onClick={toChange('notes')}>Notes</a>
-        <a href='' style={padding} onClick={toChange('users')}>Users</a>
-        <div>
-          {content()}
+    
+    <Router>
+      <div>
+        <Link to={'/'} style={padding}>Home</Link>
+        <Link to={'/notes'} style={padding}>Notes</Link>
+        <Link to={'/users'} style={padding}>Users</Link>
+      </div>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/notes' element={<Notes/>} />
+        <Route path='/users' element={<Users/>} />
+      </Routes>
+        <div id='footer' style={styles}>
+          <h1><i>footer</i></h1>
         </div>
-    </div>
+    </Router>
+    
+    
   )
 }
 
